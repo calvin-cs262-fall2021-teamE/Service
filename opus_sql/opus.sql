0ba70c41cf20f2ff73a46d978393d6554059cbb1
owenpruim <90930513+owenpruim@users.noreply.github.com>
2021-11-24 16:02:20 -0500

    Update opus.sql

--- opus_sql/opus.sql
+++ opus_sql/opus.sql
@@ -1,4 +1,4 @@
-﻿--
+--
 -- This SQL script builds a database for the OPUS app, deleting any pre-existing version.
 --
 -- @author owenpruim
@@ -23,7 +23,7 @@ CREATE TABLE Patient (
 
 CREATE TABLE Visit (
 	ID integer PRIMARY KEY,
-	visitDate date,
+	visitDate varchar(50),
 	patient integer REFERENCES Patient(registrationNumber), 
 	doctor varchar(50),
 	student varchar(50),
@@ -51,19 +51,19 @@ INSERT INTO Patient VALUES (10, 'Leia Skywalker', 'Female', '19 BBY', 'Tatooine'
 INSERT INTO Patient VALUES (11, 'James Bond', 'Male', '11/11/1920', ' Wattenscheid', 'Germany', 'English', 'English');
 INSERT INTO Patient VALUES (12, 'Indiana Jones', 'Male', '07/01/1899', 'Princeton', 'New Jersey', 'US', 'English');
 INSERT INTO Patient VALUES (13, 'Owen Pruim', 'Male', '11/17/2000', 'Grand Rapids', 'Michigan', 'US', 'English');
-INSERT INTO Patient VALUES (14), 'Adam Brink', 'Male', '7/25/2001', 'Hudsonville', 'Guacamole', 'US', 'English');
+INSERT INTO Patient VALUES (14, 'Adam Brink', 'Male', '7/25/2001', 'Hudsonville', 'Guacamole', 'US', 'English');
 INSERT INTO Patient VALUES (15, 'Josiah Ryan', 'Male', '8/6/2000', 'Little Rock', 'Arkansas', 'US', 'English');
 INSERT INTO Patient VALUES (16, 'Fitsum Maru', 'Male', '6/30/1999', 'Addis Ababa', 'Ethiopia', 'Ethiopian', 'English');
 
-INSERT INTO Visit VALUES (1, '11/11/21', 13, 'Dr. Strange', 'Spongebob', 'Color Blindness', 
+INSERT INTO Visit VALUES (18, '11/11/21', 13, 'Dr. Strange', 'Spongebob', 'Color Blindness', 
 	'Pinkeye', '11/12/21', '');
-INSERT INTO Visit VALUES (2, '10/12/1530', 1, 'Dr. Octopus', 'Plankton', 'Flu', 
+INSERT INTO Visit VALUES (17, '10/12/1530', 1, 'Dr. Octopus', 'Plankton', 'Flu', 
 	'Syphillis', '10/12/1530', 'A dog barks when his master is attacked');
 INSERT INTO Visit VALUES (1, '10/12/4000', 2, 'Dr. Banner', 'Peter Parker', 'E. coli', 
 	'', '11/12/4000', 'Theyre taking the hobbits to Isengard');
-INSERT INTO Visit VALUES (3, '06/13/4001', 3, 'Dr. Who', 'Matt Smith', 'Dengue', 
+INSERT INTO Visit VALUES (2, '06/13/4001', 3, 'Dr. Who', 'Matt Smith', 'Dengue', 
 	'', '06/14/40001', 'Going on an adventure');
-INSERT INTO Visit VALUES (1, '02/04/4500', 4, 'Dr. Frankenstein', 'Bowser', 'Malaria', 
+INSERT INTO Visit VALUES (3, '02/04/4500', 4, 'Dr. Frankenstein', 'Bowser', 'Malaria', 
 	'', '02/06/4500', 'Po-tat-toes');
 INSERT INTO Visit VALUES (4, '08/23/1720', 5, 'Dr. Doom', 'Toby Maguire', 'The Black Spot', 
 	'', '08/31/1720', 'Why is the rum gone');
